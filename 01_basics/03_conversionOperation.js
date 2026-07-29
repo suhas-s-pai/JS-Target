6a786709d5c4f0e46ba287c3efea5ba96df4c655
suhas-s-pai <suhaspai13@gmail.com>
2026-07-29 12:51:06 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = true
+
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1; false => =0;
+
+let isLoggedIn = "Suhas S Pai"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false ;
+// "" => false;
+// "Suhas S Pai" => true;
+
+let kapi = "Sakkar"
+let chai = Number(kapi)
+console.log(chai)
+console.log(typeof chai)

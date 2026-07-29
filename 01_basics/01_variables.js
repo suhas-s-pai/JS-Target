64949343bca5b3ce120f66d6a81222af234b2c68
suhas-s-pai <suhaspai13@gmail.com>
2026-07-29 09:59:25 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 55443
+let accountEmail = "Suhas@gmail.com"
+var accountPassword = "1234"
+accountCity = "Kummble"
+
+//accountId = 2
+accountEmail = "Rahul@gmail.com"
+accountPassword = "1313"
+accountCity = "Goshripuram"
+let accountState
+//console.log(accountCity)
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" //treate all js code asnewer version
+
+//alert( 3 + 3 ) //we are using node js, not browser
+//console.log( 3 + 3);console.log("Suhas S Pai")
+
+let name = "Suhas S Pai"
+let age = 20
+let isLogged = true
+
+// number => 2 to power 53
+// bigInt
+// string => "Suhas S Pai"
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof null);

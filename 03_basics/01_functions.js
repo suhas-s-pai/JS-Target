5802be760ccc0d561e8ed18828584b340519af2f
suhas-s-pai <suhaspai13@gmail.com>
2026-08-03 09:32:24 +0000

    Function part one

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,63 @@
+
+// function sayMyName(){
+//     console.log("S")
+// console.log("U")
+// console.log("H")
+// console.log("A")
+// console.log("S")
+// }
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     let result = number1 + number2
+//         console.log("Suhas")
+
+//     return result
+//  }
+
+// const result = addTwoNumbers(3, 4)
+
+// console.log("result: ",result)
+
+// function loginUserMessage(username = "suhu"){
+//     if( !username)
+//     {
+//         console.log("Please enter the username.")
+//         return
+//     }
+//     return `${username} just logged in`
+
+// }
+
+// console.log(loginUserMessage("Suhas"))
+
+//  function calculateCartPrice(val1, val2,...num1){
+//     return num1
+//  }
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+// const user = {
+//     username: "Suhas",
+//     price: 199
+// }
+
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+
+// }
+
+// // handleObject(user)
+// handleObject({
+//     username: "Suhas",
+//     price: 399
+// }) 
+
+const myNewArray = [200, 400, 100, 600]
+
+function thirdArray(getArray){
+    return getArray[3]
+}
+console.log(thirdArray(myNewArray))
+
